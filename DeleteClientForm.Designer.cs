d50c9f833c0d1e4a0bdbaea2c3cb3732327ac3e6
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-18 16:06:03 +0200

    btnDelete, txtOrderNumber added.

--- DeleteClientForm.Designer.cs
+++ DeleteClientForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace oop8_orders
+namespace oop8_orders
 {
     partial class DeleteClientForm
     {
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.btnHome = new System.Windows.Forms.Button();
+            this.labelDeleteInfo = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtOrderNumber = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnHome
             // 
             this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnHome.Location = new System.Drawing.Point(427, 22);
+            this.btnHome.Location = new System.Drawing.Point(205, 66);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(160, 49);
             this.btnHome.TabIndex = 10;
@@ -42,20 +45,59 @@
             this.btnHome.UseVisualStyleBackColor = true;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
+            // labelDeleteInfo
+            // 
+            this.labelDeleteInfo.AutoSize = true;
+            this.labelDeleteInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDeleteInfo.Location = new System.Drawing.Point(22, 22);
+            this.labelDeleteInfo.Name = "labelDeleteInfo";
+            this.labelDeleteInfo.Size = new System.Drawing.Size(381, 20);
+            this.labelDeleteInfo.TabIndex = 11;
+            this.labelDeleteInfo.Text = "Для видалення введіть номер замовлення:";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Salmon;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(26, 66);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(160, 44);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Видалити";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // txtOrderNumber
+            // 
+            this.txtOrderNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtOrderNumber.Location = new System.Drawing.Point(409, 19);
+            this.txtOrderNumber.Name = "txtOrderNumber";
+            this.txtOrderNumber.Size = new System.Drawing.Size(83, 26);
+            this.txtOrderNumber.TabIndex = 13;
+            // 
             // DeleteClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(509, 132);
+            this.Controls.Add(this.txtOrderNumber);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.labelDeleteInfo);
             this.Controls.Add(this.btnHome);
+            this.MaximumSize = new System.Drawing.Size(525, 171);
+            this.MinimumSize = new System.Drawing.Size(525, 171);
             this.Name = "DeleteClientForm";
             this.Text = "DeleteClientForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Label labelDeleteInfo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtOrderNumber;
     }
 }
